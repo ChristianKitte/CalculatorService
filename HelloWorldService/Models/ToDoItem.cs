508b3823f574cd1fa96a09161349880f5611c97a
Christian Kitte <chkitte@web.de>
2020-12-21 23:25:00 +0100

    Projektdateien hinzufügen.

---
+++ HelloWorldService/Models/ToDoItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorldService.Models
+{
+    public class ToDoItem
+    {
+        public string Name = "Christian Kitte";
+    }
+}
